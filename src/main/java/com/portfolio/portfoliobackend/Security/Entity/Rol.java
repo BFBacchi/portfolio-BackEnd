5cf00cd4bf5d28a0bed0adf621b133c4e91ea7bf
BFBacchi <bfbacchi@gmail.com>
2023-05-11 10:26:33 -0300

    Se implementa JWT

---
+++ src/main/java/com/portfolio/portfoliobackend/Security/Entity/Rol.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.portfoliobackend.Security.Entity;
+
+import com.portfolio.portfoliobackend.Security.Enums.RolNombre;
+import com.sun.istack.NotNull;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import org.hibernate.annotations.Entity;
+
+import org.springframework.data.annotation.Id;
+
+
+@Entity
+
+
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private RolNombre rolNombre;
+
+    public Rol() {
+    }
+
+    public Rol(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public RolNombre getRolNombre() {
+        return rolNombre;
+    }
+
+    public void setRolNombre(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+    
+    
+}
